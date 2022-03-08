8d064359ce78751ade4d705802ac6d3b8590e2e1
dhanushreddy291 <29dhanushreddy@gmail.com>
2022-03-08 05:00:22 +0000

    Checking old Codebase

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
+import tweepy, os, urllib.request
+from PIL import Image
+from dotenv import load_dotenv
+from datetime import datetime
+load_dotenv()
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+        return "The, Tweet was successful "
